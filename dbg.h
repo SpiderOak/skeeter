53632a632fe58a86e7143c495706a7e00af3d5be
dougfort <dougfort@spideroak.com>
2012-06-26 16:07:12 -0400

    add spaces to dbg.h messages

--- dbg.h
+++ dbg.h
@@ -23,14 +23,14 @@
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
 #define log_err(M, ...) fprintf(stderr, \
-                              "[ERROR] %s:%d: errno: %s" M "\n", \
+                              "[ERROR] %s:%d: errno: %s " M "\n", \
                               __FILE__, \
                               __LINE__, \
                               clean_errno(), \
                               ##__VA_ARGS__)
 
 #define log_warn(M, ...) fprintf(stderr, \
-                                 "[WARN] %s:%d: errno: %s" M "\n", \
+                                 "[WARN] %s:%d: errno: %s " M "\n", \
                                  __FILE__, \
                                  __LINE__, \
                                  clean_errno(), \
